825bc40c03971307a4d33321ac0dda9459ab8b1e
Cosmas-Emeka <cosmasemeka11@gmail.com>
2023-05-11 12:45:36 +0100

    Positioned mobile twitter icon

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ var algoFollowing = document.getElementById("algoFollowing");
 function algoForYou() {
     algoForYou.width = "100%"
 }
+
+function truncateString(str, num) {
+    if (str.length > num) {
+     let truncate = str.slice(0, num) + "...";
+      return truncate;
+    }else {
+    return str
+    }
+  }
