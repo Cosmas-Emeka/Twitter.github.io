8fd064f0a4be046066b42e86f1ab9725f4f980c5
Cosmas-Emeka <cosmasemeka11@gmail.com>
2023-05-15 19:10:50 +0100

    Messages section on the go

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createPostWrapper = document.getElementById("createPostWrapper");
-var postTweet = document.getElementById("postTweet");
-
+var msg = document.getElementById("msg");
+var angleUp = document.getElementById("angleUp");
+var angleDown = document.getElementById("angleDown");
 
 function addTweet() {
   createPostWrapper.style.left = "0";
@@ -9,6 +10,20 @@ function addTweet() {
 
 function hideTweet() {
   createPostWrapper.style.left = "-100%";
+}
+
+
+function msgUp() {
+  msg.style.bottom = "-0.7rem";
+  angleUp.style.display = "none";
+  angleDown.style.display = "flex";
+}
+
+
+function msgDown() {
+  msg.style.bottom = "-27rem";
+  angleUp.style.display = "flex";
+  angleDown.style.display = "none";
 }
 
 
